4293f3d6d8d84443b9ace540a20de4829aa0639e
Anirudh Parvatikar <71212295+anirudhp06@users.noreply.github.com>
2022-07-03 19:15:57 +0530

    Added Text Box
    Added Text Box to display the outstanding bill & added functionality to some buttons...

--- Client.java
+++ Client.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 import java.sql.*;
-
-import javax.print.attribute.standard.JobKOctetsSupported;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
+import javax.swing.border.*;
 public class Client {
     public static void main(String[] args){
         JFrame f1=new JFrame();
@@ -32,10 +32,10 @@ public class Client {
         JLabel starters=new JLabel("Starters");
         starters.setBounds(10,200,100,100);
         f1.add(starters);
-        JCheckBox ch1=new JCheckBox("No starters(0/-)");
+        JCheckBox ch1=new JCheckBox("No starters 0/-");
         ch1.setBounds(10, 270, 250, 30);
         f1.add(ch1);
-        JCheckBox ch2=new JCheckBox("Gobi Manchurian(150/-)");
+        JCheckBox ch2=new JCheckBox("Gobi Manchurian 150/-");
         ch2.setBounds(10, 300, 250, 30);
         ch2.addItemListener(new ItemListener(){
             public void itemStateChanged(ItemEvent e){
@@ -45,7 +45,7 @@ public class Client {
             }
         });
         f1.add(ch2);
-        JCheckBox ch3=new JCheckBox("Paneer Manchurian(170/-)");
+        JCheckBox ch3=new JCheckBox("Paneer Manchurian 170/-");
         ch3.setBounds(10, 330, 250, 30);
         ch3.addItemListener(new ItemListener(){
             public void itemStateChanged(ItemEvent e){
@@ -55,7 +55,7 @@ public class Client {
             }
         });
         f1.add(ch3);
-        JCheckBox ch4=new JCheckBox("Baby Corn Manchurian(160/-)");
+        JCheckBox ch4=new JCheckBox("Baby Corn Manchurian 160/-");
         ch4.setBounds(10, 360, 250, 30);
         ch4.addItemListener(new ItemListener(){
             public void itemStateChanged(ItemEvent e){
@@ -83,5 +83,27 @@ public class Client {
             }
         });
         f1.add(submit);
+        JTextArea ta1=new JTextArea(10,25);
+        ta1.setBounds(500, 150, 600, 200);
+		ta1.setLineWrap(true);
+		Border b3=BorderFactory.createLineBorder(Color.GREEN,10);
+		ta1.setBorder(b3);
+		ta1.setFont(new Font("Times New Roman",Font.BOLD,20));
+		f1.add(ta1);
+        JButton calc =new JButton("CALCULATE BILL");
+        calc.setBounds(10,500,200,20);
+        calc.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                int bill=150;
+                String msg="Online Seat Booking:150/-\n",part="";
+                ta1.setText(msg);
+                if(ch1.isSelected()){bill=150;ta1.setText(msg+"\n Total Bill:"+bill+"/-");}
+                if(ch2.isSelected()){bill+=150;part+=ch2.getText()+"\n";ta1.setText(msg+part+"\n Total Bill:"+bill);}
+                if(ch3.isSelected()){bill+=170;part+=ch3.getText()+"\n";ta1.setText(msg+part+"\n Total Bill:"+bill);}
+                if(ch4.isSelected()){bill+=160;part+=ch4.getText()+"\n";ta1.setText(msg+part+"\n Total Bill:"+bill);}
+                //ta1.setText("Total Bill:"+bill);
+            }
+        });
+        f1.add(calc);
     }
 }
