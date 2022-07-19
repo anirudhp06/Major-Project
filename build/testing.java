aca541fa1b1b7f083986d631456e04037f7748f7
Anirudh Parvatikar <71212295+anirudhp06@users.noreply.github.com>
2022-07-19 21:36:45 +0530

    Trying jar files...

---
+++ build/testing.java
@@ -0,0 +1,12 @@
+//package build;
+import javax.swing.*;
+import java.awt.*;
+public class testing {
+    public static void main(String[] args) {
+        JFrame f1=new JFrame();
+        f1.setTitle("Hello");
+        f1.setVisible(true);
+        f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f1.setBounds(10,20,500,520);
+    }
+}
